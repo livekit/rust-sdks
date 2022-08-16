b59606b989dece4e72cba236e9af65f4b85f22bd
Théo Monnom <theo.monnom@outlook.com>
2022-08-16 15:41:51 +0200

    Add basic project boilerplate for iOS & Android ( To use platform specific code )

---
+++ examples/ios-project/ios-project/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  ios-project
+//
+//  Created by Théo Monnom on 15/08/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ examples/ios-project/ios-project/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ios-project
+//
+//  Created by Théo Monnom on 15/08/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
