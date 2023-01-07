a26df1d147e08d89c6e1666c64b5eb26a70cd7c9
Théo Monnom <theo.monnom@outlook.com>
2023-01-07 18:27:02 +0100

    upgrade webrtc version

--- webrtc-sys/build.rs
+++ webrtc-sys/build.rs
@@ -8,7 +8,7 @@ use std::path;
 use std::process::Command;
 use tar::Archive;
 
-const WEBRTC_TAG: &str = "m104.5112.06";
+const WEBRTC_TAG: &str = "m104.5112.07";
 
 fn download_prebuilt(
     target_os: &str,
