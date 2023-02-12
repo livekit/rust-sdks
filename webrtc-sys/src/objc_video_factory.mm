4565f587c71cddc0fca5374bc12eef4ef28800ce
Théo Monnom <theo.monnom@outlook.com>
2023-02-12 20:17:46 +0100

    license headers

--- webrtc-sys/src/objc_video_factory.mm
+++ webrtc-sys/src/objc_video_factory.mm
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "livekit/objc_video_factory.h"
 
 #import <sdk/objc/components/video_codec/RTCVideoDecoderFactoryH264.h>
@@ -9,13 +25,11 @@ namespace livekit {
 
 std::unique_ptr<webrtc::VideoEncoderFactory> CreateObjCVideoEncoderFactory() {
   // TODO(theomonnom): Simulcast?
-  return webrtc::ObjCToNativeVideoEncoderFactory(
-      [[RTCVideoEncoderFactoryH264 alloc] init]);
+  return webrtc::ObjCToNativeVideoEncoderFactory([[RTCVideoEncoderFactoryH264 alloc] init]);
 }
 
 std::unique_ptr<webrtc::VideoDecoderFactory> CreateObjCVideoDecoderFactory() {
-  return webrtc::ObjCToNativeVideoDecoderFactory(
-      [[RTCVideoDecoderFactoryH264 alloc] init]);
+  return webrtc::ObjCToNativeVideoDecoderFactory([[RTCVideoDecoderFactoryH264 alloc] init]);
 }
 
-}
+}  // namespace livekit
