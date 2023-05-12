f2b4373ba40ecd52fc47072b7082667b6ee5325d
Aaron Spiegel <spiegela@gmail.com>
2023-05-12 05:48:56 -0500

    Add proto3 optional to prost build (#65)
    * add proto3 optional to prost build
    
    * also add optional proto flag to livekit_ffi
    
    ---------
    
    Co-authored-by: Théo Monnom <theo.monnom@outlook.com>

--- livekit-protocol/build.rs
+++ livekit-protocol/build.rs
@@ -2,6 +2,7 @@ use std::io::Result;
 
 fn main() -> Result<()> {
     let mut prost_build = prost_build::Config::new();
+    prost_build.protoc_arg("--experimental_allow_proto3_optional");
     prost_build.compile_protos(
         &[
             "protocol/livekit_egress.proto",
