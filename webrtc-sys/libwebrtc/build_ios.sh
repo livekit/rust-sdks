8697b0162280167329bfb865aefb73545bce61ba
Théo Monnom <theo.monnom@outlook.com>
2023-06-22 16:36:31 +0200

    ios & android webrtc builds

---
+++ webrtc-sys/libwebrtc/build_ios.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+arch="arm64"
+profile="release"
+environment="device"
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --environment)
+      environment="$2"
+      if [ "$environment" != "device" ] && [ "$environment" != "simulator" ]; then
+        echo "Error: Invalid value for --environment. Must be 'device' or 'simulator'."
+        exit 1
+      fi
+      shift 2
+      ;;
+    --profile)
+      profile="$2"
+      if [ "$profile" != "debug" ] && [ "$profile" != "release" ]; then
+        echo "Error: Invalid value for --profile. Must be 'debug' or 'release'."
+        exit 1
+      fi
+      shift 2
+      ;;
+    *)
+      echo "Error: Unknown argument '$1'"
+      exit 1
+      ;;
+  esac
+done
+
+echo "Building LiveKit WebRTC"
+echo "Arch: $arch"
+echo "Profile: $profile"
+echo "Environment: $environment"
+
+if [ ! -e "$(pwd)/depot_tools" ]
+then
+  git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+
+export COMMAND_DIR=$(cd $(dirname $0); pwd)
+export PATH="$(pwd)/depot_tools:$PATH"
+
+export OUTPUT_DIR="$(pwd)/src/out-$arch-$profile"
+export ARTIFACTS_DIR="$(pwd)/ios-$arch-$profile"
+
+if [ "$environment" = "simulator" ]; then
+  export OUTPUT_DIR="$OUTPUT_DIR-simulator"
+  export ARTIFACTS_DIR="$ARTIFACTS_DIR-simulator"
+fi
+
+if [ ! -e "$(pwd)/src" ]
+then
+  gclient sync -D --no-history
+fi
+
+cd src
+git apply "$COMMAND_DIR/patches/add_license_dav1d.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
+git apply "$COMMAND_DIR/patches/ssl_verify_callback_with_native_handle.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
+git apply "$COMMAND_DIR/patches/fix_mocks.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
+cd ..
+
+mkdir -p "$ARTIFACTS_DIR/lib"
+
+debug="false"
+if [ "$profile" = "debug" ]; then
+  debug="true"
+fi
+
+# generate ninja files
+gn gen "$OUTPUT_DIR" --root="src" \
+  --args="is_debug=$debug \
+  enable_dsyms=$debug \
+  target_os=\"ios\" \
+  target_cpu=\"$arch\" \
+  target_environment=\"$environment\" \
+  treat_warnings_as_errors=false \
+  ios_enable_code_signing=false \
+  rtc_enable_protobuf=false \
+  rtc_include_tests=false \
+  rtc_build_examples=false \
+  rtc_build_tools=false \
+  rtc_libvpx_build_vp9=true \
+  is_component_build=false \
+  enable_stripping=true \
+  rtc_enable_symbol_export=true \
+  rtc_enable_objc_symbol_export=false \
+  rtc_use_h264=false \
+  use_custom_libcxx=false \
+  clang_use_chrome_plugins=false \
+  use_rtti=true \
+  use_lld=false"
+
+# build static library
+ninja -C "$OUTPUT_DIR" :default \
+  api/audio_codecs:builtin_audio_decoder_factory \
+  api/task_queue:default_task_queue_factory \
+  sdk:native_api \
+  sdk:default_codec_factory_objc \
+  pc:peerconnection \
+  sdk:videocapture_objc \
+  sdk:framework_objc
+
+# make libwebrtc.a
+# don't include nasm
+ar -rc "$ARTIFACTS_DIR/lib/libwebrtc.a" `find "$OUTPUT_DIR/obj" -name '*.o' -not -path "*/third_party/nasm/*"`
+
+python3 "./src/tools_webrtc/libs/generate_licenses.py" \
+  --target :webrtc "$OUTPUT_DIR" "$OUTPUT_DIR"
+
+cp "$OUTPUT_DIR/obj/webrtc.ninja" "$ARTIFACTS_DIR"
+cp "$OUTPUT_DIR/args.gn" "$ARTIFACTS_DIR"
+cp "$OUTPUT_DIR/LICENSE.md" "$ARTIFACTS_DIR"
+
+cd src
+find . -name "*.h" -print | cpio -pd "$ARTIFACTS_DIR/include"
+
