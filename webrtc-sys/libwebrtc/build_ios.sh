d4d1171e97ec758b0e7d9309419e33fe264b00c6
Théo Monnom <theo.monnom@outlook.com>
2023-06-25 00:32:47 +0200

    fix: webrtc licenses generation (#95)

--- webrtc-sys/libwebrtc/build_ios.sh
+++ webrtc-sys/libwebrtc/build_ios.sh
@@ -64,7 +64,7 @@ then
 fi
 
 cd src
-git apply "$COMMAND_DIR/patches/add_license_dav1d.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
+git apply "$COMMAND_DIR/patches/add_licenses.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
 git apply "$COMMAND_DIR/patches/ssl_verify_callback_with_native_handle.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
 git apply "$COMMAND_DIR/patches/fix_mocks.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
 cd ..
