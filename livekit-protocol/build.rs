1f483454d2ce4512a8293ccb3ae2021f9431d8ce
Théo Monnom <theo.monnom@outlook.com>
2023-06-26 23:38:28 +0200

    add protojson spec support

--- livekit-protocol/build.rs
+++ livekit-protocol/build.rs
@@ -1,17 +1,39 @@
-use std::io::Result;
+use std::{env, io::Result, path::PathBuf};
 
 fn main() -> Result<()> {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let descriptor_path = PathBuf::from(out_dir).join("proto_descriptor.bin");
+
+    let proto_files = &[
+        "protocol/livekit_egress.proto",
+        "protocol/livekit_rtc.proto",
+        "protocol/livekit_room.proto",
+        "protocol/livekit_webhook.proto",
+        "protocol/livekit_models.proto",
+    ];
+
+    for file in proto_files {
+        println!("cargo:rerun-if-changed={}", file);
+    }
+
     let mut prost_build = prost_build::Config::new();
-    prost_build.protoc_arg("--experimental_allow_proto3_optional");
-    prost_build.compile_protos(
-        &[
-            "protocol/livekit_egress.proto",
-            "protocol/livekit_rtc.proto",
-            "protocol/livekit_room.proto",
-            "protocol/livekit_webhook.proto",
-            "protocol/livekit_models.proto",
-        ],
-        &["protocol/"],
-    )?;
+
+    if cfg!(feature = "json") {
+        prost_build.extern_path(".google.protobuf", "::pbjson_types");
+    }
+
+    prost_build
+        .file_descriptor_set_path(&descriptor_path) // Needed for pbjson
+        .compile_well_known_types()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile_protos(proto_files, &["protocol/"])?;
+
+    if cfg!(feature = "json") {
+        let descriptor_set = std::fs::read(descriptor_path)?;
+        pbjson_build::Builder::new()
+            .register_descriptors(&descriptor_set)?
+            .build(&[".livekit"])?;
+    }
+
     Ok(())
 }
--- livekit-protocol/src/lib.rs
+++ livekit-protocol/src/lib.rs
@@ -1,8 +1,21 @@
-pub mod observer;
 pub mod enum_dispatch;
+pub mod observer;
 
-pub mod livekit {
+#[cfg(not(feature = "json"))]
+mod livekit {
+    pub mod google {
+        pub mod protobuf {
+            include!(concat!(env!("OUT_DIR"), "/google.protobuf.rs"));
+        }
+    }
+
     include!(concat!(env!("OUT_DIR"), "/livekit.rs"));
+}
+
+#[cfg(feature = "json")]
+mod livekit {
+    include!(concat!(env!("OUT_DIR"), "/livekit.rs"));
+    include!(concat!(env!("OUT_DIR"), "/livekit.serde.rs"));
 }
 
 pub use livekit::*;
