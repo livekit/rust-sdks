9ca370d03cddc6a5afa7c44c0328e224515768e5
Théo Monnom <theo.monnom@outlook.com>
2023-07-03 15:14:32 +0200

    fix: correctly wait publisher connection (#108)

--- examples/basic_room/src/main.rs
+++ examples/basic_room/src/main.rs
@@ -11,8 +11,19 @@ async fn main() {
     let url = env::var("LIVEKIT_URL").expect("LIVEKIT_URL is not set");
     let token = env::var("LIVEKIT_TOKEN").expect("LIVEKIT_TOKEN is not set");
 
-    let (room, mut rx) = Room::connect(&url, &token, RoomOptions::default()).await.unwrap();
+    let (room, mut rx) = Room::connect(&url, &token, RoomOptions::default())
+        .await
+        .unwrap();
     log::info!("Connected to room: {} - {}", room.name(), room.sid());
+
+    room.local_participant()
+        .publish_data(
+            "Hello world".to_owned().into_bytes(),
+            DataPacketKind::Reliable,
+            Default::default(),
+        )
+        .await
+        .unwrap();
 
     while let Some(msg) = rx.recv().await {
         log::info!("Event: {:?}", msg);
