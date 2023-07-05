cf37b33e7c465fd8d2164536e188802296c267d3
Théo Monnom <theo.monnom@outlook.com>
2023-07-05 13:55:43 +0200

    use shared lib
    simplify things

--- examples/mobile/build_ios_framework.sh
+++ examples/mobile/build_ios_framework.sh
@@ -3,8 +3,8 @@ cargo build --release --target aarch64-apple-ios
 cargo build --release --target aarch64-apple-ios-sim
 
 xcodebuild -create-xcframework \
-  -library ../target/aarch64-apple-ios/release/libmobile.a \
+  -library ../target/aarch64-apple-ios/release/libmobile.dylib \
   -headers ./include/ \
-  -library ../target/aarch64-apple-ios-sim/release/libmobile.a \
+  -library ../target/aarch64-apple-ios-sim/release/libmobile.dylib \
   -headers ./include/ \
   -output ios/MobileExample.xcframework
