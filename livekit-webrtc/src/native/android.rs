023ede7baea608a31c5ce0e86b2f6a1671d53d74
Théo Monnom <theo.monnom@outlook.com>
2023-07-06 13:26:58 +0200

    init_android VM

--- livekit-webrtc/src/native/android.rs
+++ livekit-webrtc/src/native/android.rs
@@ -1,3 +1,7 @@
-pub fn initialize_android(vm: jni::JavaVM) {
-    vm.get_java_vm_pointer()
+use webrtc_sys::android::ffi as sys_android;
+
+pub fn initialize_android(vm: &jni::JavaVM) {
+    unsafe {
+        sys_android::init_android(vm.get_java_vm_pointer() as *mut _);
+    }
 }
