19d07c35e8328fdd84ca147ce3080466d4c6abf3
Théo Monnom <theo.monnom@outlook.com>
2023-07-07 14:41:29 +0200

    fix conflicts

--- livekit-ffi/build.rs
+++ livekit-ffi/build.rs
@@ -1,25 +1,7 @@
 use std::env;
-use std::io::Result;
 
-fn main() -> Result<()> {
-    let mut prost_build = prost_build::Config::new();
-    prost_build.protoc_arg("--experimental_allow_proto3_optional");
-    prost_build.compile_protos(
-        &[
-            "protocol/ffi.proto",
-            "protocol/handle.proto",
-            "protocol/room.proto",
-            "protocol/track.proto",
-            "protocol/participant.proto",
-            "protocol/video_frame.proto",
-            "protocol/audio_frame.proto",
-        ],
-        &["protocol/"],
-    )?;
-
+fn main() {
     if env::var("CARGO_CFG_TARGET_OS").unwrap() == "android" {
         webrtc_sys_build::configure_jni_symbols().unwrap();
     }
-
-    Ok(())
 }
