71ef76c582ac393248142e9cecb8d7aaedfd2312
Théo Monnom <theo.monnom@outlook.com>
2023-07-10 15:33:09 +0200

    fix: android crashes + working example (#122)

---
+++ examples/mobile/android/app/src/main/java/io/livekit/rustexample/MainActivity.kt
@@ -0,0 +1,46 @@
+package io.livekit.rustexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.livekit.rustexample.ui.theme.RustexampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val app = App()
+        app.connect(
+                "ws://localhost:7880",
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5MDY2MTMyODgsImlzcyI6IkFQSVRzRWZpZFpqclFvWSIsIm5hbWUiOiJuYXRpdmUiLCJuYmYiOjE2NzI2MTMyODgsInN1YiI6Im5hdGl2ZSIsInZpZGVvIjp7InJvb20iOiJ0ZXN0Iiwicm9vbUFkbWluIjp0cnVlLCJyb29tQ3JlYXRlIjp0cnVlLCJyb29tSm9pbiI6dHJ1ZSwicm9vbUxpc3QiOnRydWV9fQ.uSNIangMRu8jZD5mnRYoCHjcsQWCrJXgHCs0aNIgBFY"
+        )
+
+        setContent {
+            RustexampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colorScheme.background
+                ) { Greeting("Android") }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RustexampleTheme { Greeting("Android") }
+}
--- examples/mobile/build_android.sh
+++ examples/mobile/build_android.sh
@@ -1,1 +1,8 @@
+cargo ndk -t armeabi-v7a b --release
 cargo ndk -t arm64-v8a b --release
+
+mkdir -p android/app/src/main/jniLibs/armeabi-v7a
+mkdir -p android/app/src/main/jniLibs/arm64-v8a
+
+mv ../target/armv7-linux-androideabi/release/libmobile.so android/app/src/main/jniLibs/armeabi-v7a/libmobile.so
+mv ../target/aarch64-linux-android/release/libmobile.so android/app/src/main/jniLibs/arm64-v8a/libmobile.so
