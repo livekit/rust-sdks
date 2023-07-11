69f6d01a642ae868d781f3a5fccbe59068205246
Théo Monnom <theo.monnom@outlook.com>
2023-07-11 13:55:24 +0200

    Update build_android.sh

--- webrtc-sys/libwebrtc/build_android.sh
+++ webrtc-sys/libwebrtc/build_android.sh
@@ -53,7 +53,7 @@ then
 fi
 
 cd src
-git apply "$COMMAND_DIR/patches/add_licenses.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
+# git apply "$COMMAND_DIR/patches/add_licenses.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
 git apply "$COMMAND_DIR/patches/ssl_verify_callback_with_native_handle.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
 git apply "$COMMAND_DIR/patches/fix_mocks.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
 git apply "$COMMAND_DIR/patches/android_use_libunwind.patch" -v --ignore-space-change --ignore-whitespace --whitespace=nowarn
