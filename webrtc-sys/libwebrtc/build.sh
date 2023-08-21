78f76623c45733840fe78aa7dbcced8d267d7aee
Gustavo Buzogany Eboli <gustavo@lighttwist.com>
2023-08-21 15:20:41 +0200

    fix macos build

--- webrtc-sys/libwebrtc/build.sh
+++ webrtc-sys/libwebrtc/build.sh
@@ -1,3 +1,9 @@
-./build_linux.sh --arch x64
-mkdir -p lib
-cp -r linux-x64-release/* .
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    ./build_macos.sh --arch arm64
+    mkdir -p lib
+    cp -r mac-arm64-release/* .
+else
+    ./build_linux.sh --arch x64
+    mkdir -p lib
+    cp -r linux-x64-release/* .
+fi
