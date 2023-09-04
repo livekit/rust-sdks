c6f59a059446192a20dd5be6d87fe55afc6a27ce
Théo Monnom <theo.monnom@outlook.com>
2023-09-04 16:54:29 -0700

    ffi v0.3.0

--- livekit-ffi/src/server/requests.rs
+++ livekit-ffi/src/server/requests.rs
@@ -543,12 +543,12 @@ fn on_e2ee_request(
 
             proto::e2ee_response::Message::SetSharedKey(proto::SetSharedKeyResponse {})
         }
-        proto::e2ee_request::Message::RachetSharedKey(request) => {
+        proto::e2ee_request::Message::RatchetSharedKey(request) => {
             let new_key = e2ee_manager
                 .key_provider()
                 .and_then(|key_provider| key_provider.ratchet_shared_key(request.key_index));
 
-            proto::e2ee_response::Message::RachetSharedKey(proto::RachetSharedKeyResponse {
+            proto::e2ee_response::Message::RatchetSharedKey(proto::RatchetSharedKeyResponse {
                 new_key,
             })
         }
@@ -565,13 +565,13 @@ fn on_e2ee_request(
             }
             proto::e2ee_response::Message::SetKey(proto::SetKeyResponse {})
         }
-        proto::e2ee_request::Message::RachetKey(request) => {
+        proto::e2ee_request::Message::RatchetKey(request) => {
             let identity = request.participant_identity.try_into().unwrap();
             let new_key = e2ee_manager
                 .key_provider()
                 .and_then(|key_provider| key_provider.ratchet_key(&identity, request.key_index));
 
-            proto::e2ee_response::Message::RachetKey(proto::RachetKeyResponse { new_key })
+            proto::e2ee_response::Message::RatchetKey(proto::RatchetKeyResponse { new_key })
         }
         proto::e2ee_request::Message::GetKey(request) => {
             let identity = request.participant_identity.try_into().unwrap();
