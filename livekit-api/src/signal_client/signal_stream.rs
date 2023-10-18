0085097caa9486d29b5119aa7fa8238999f8cf9a
Théo Monnom <theo.monnom@outlook.com>
2023-10-18 11:18:39 -0700

    Update signal_stream.rs

--- livekit-api/src/signal_client/signal_stream.rs
+++ livekit-api/src/signal_client/signal_stream.rs
@@ -22,6 +22,7 @@ use tokio::sync::{mpsc, oneshot};
 use tokio::task::JoinHandle;
 use tokio_tungstenite::tungstenite::Message;
 use tokio_tungstenite::{connect_async, MaybeTlsStream, WebSocketStream};
+use url::form_urlencoded;
 
 type WebSocket = WebSocketStream<MaybeTlsStream<TcpStream>>;
 
@@ -60,10 +61,25 @@ impl SignalStream {
         mpsc::UnboundedReceiver<Box<proto::signal_response::Message>>,
     )> {
         {
-            // Don't log the access token
-            let mut durl = url.clone();
-            durl.query_pairs_mut().clear();
-            log::info!("connecting to {}", durl);
+            // Don't log sensitive info
+            let mut url = url.clone();
+            let filtered_pairs: Vec<_> = url
+                .query_pairs()
+                .filter(|(key, _)| key != "access_token")
+                .map(|(k, v)| (k.into_owned(), v.into_owned()))
+                .collect();
+
+            {
+                let mut query_pairs = url.query_pairs_mut();
+                query_pairs.clear();
+                for (key, value) in filtered_pairs {
+                    query_pairs.append_pair(&key, &value);
+                }
+
+                query_pairs.append_pair("access_token", "...");
+            }
+
+            log::info!("connecting to {}", url);
         }
 
         // Automatically switch to websocket scheme when using http
