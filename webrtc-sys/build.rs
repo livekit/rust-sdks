bc7a3bf38301d009c0699973b7182b02db0ce024
Théo Monnom <theo.monnom@outlook.com>
2023-12-02 23:30:53 +0100

    Update build.rs

--- webrtc-sys/build.rs
+++ webrtc-sys/build.rs
@@ -125,7 +125,7 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib=dwmapi");
             println!("cargo:rustc-link-lib=dylib=shcore");
 
-            builder.flag("/std:c++20").flag("/EHsc");
+            builder.flag("/std:c++latest").flag("/EHsc").flag("/W");
         }
         "linux" => {
             println!("cargo:rustc-link-lib=dylib=Xext");
