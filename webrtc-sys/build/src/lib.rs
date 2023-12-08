4203594c33b4ff39a0ece90b54c233014babde63
Théo Monnom <theo.monnom@outlook.com>
2023-12-08 23:22:00 +0100

    update webrtc to webrtc-d5afc4b-2 (#255)

--- webrtc-sys/build/src/lib.rs
+++ webrtc-sys/build/src/lib.rs
@@ -26,7 +26,7 @@ use regex::Regex;
 use reqwest::StatusCode;
 
 pub const SCRATH_PATH: &str = "livekit_webrtc";
-pub const WEBRTC_TAG: &str = "webrtc-d5afc4b";
+pub const WEBRTC_TAG: &str = "webrtc-d5afc4b-2";
 pub const IGNORE_DEFINES: [&str; 2] = ["CR_CLANG_REVISION", "CR_XCODE_VERSION"];
 
 pub fn target_os() -> String {
