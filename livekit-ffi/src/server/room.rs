a4894f8efa4369552d70c9bcf2fc040a1793abaa
Théo Monnom <theo.8bits@gmail.com>
2023-12-21 14:27:57 +0100

    Update room.rs

--- livekit-ffi/src/server/room.rs
+++ livekit-ffi/src/server/room.rs
@@ -205,20 +205,19 @@ impl RoomInner {
         let destination_sids = publish.destination_sids;
         let async_id = server.next_id();
 
-        self.data_tx
-            .send(FfiDataPacket {
-                payload: DataPacket {
-                    payload: data.to_vec(), // Avoid copy?
-                    kind: kind.into(),
-                    topic: publish.topic,
-                    destination_sids: destination_sids
-                        .into_iter()
-                        .map(|str| str.try_into().unwrap())
-                        .collect(),
-                },
-                async_id,
-            })
-            .map_err(|_| FfiError::InvalidRequest("failed to send data packet".into()))?;
+        // Ignore result, if the channel is closed, it means the room has been closed
+        let _ = self.data_tx.send(FfiDataPacket {
+            payload: DataPacket {
+                payload: data.to_vec(), // Avoid copy?
+                kind: kind.into(),
+                topic: publish.topic,
+                destination_sids: destination_sids
+                    .into_iter()
+                    .map(|str| str.try_into().unwrap())
+                    .collect(),
+            },
+            async_id,
+        });
 
         Ok(proto::PublishDataResponse { async_id })
     }
