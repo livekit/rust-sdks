ae1d7927697df725ed27a559a289a4d3f5ac636e
Neil Dwyer <neildwyer1991@gmail.com>
2024-07-19 10:40:33 -0700

    empty string for deleted keys

--- livekit/src/room/utils/mod.rs
+++ livekit/src/room/utils/mod.rs
@@ -4,17 +4,25 @@ pub fn calculate_changed_attributes(
     old_attributes: HashMap<String, String>,
     new_attributes: HashMap<String, String>,
 ) -> HashMap<String, String> {
-    let old_keys: Vec<_> = old_attributes.keys().collect();
-    let new_keys: Vec<_> = new_attributes.keys().collect();
-    let all_keys: Vec<_> = old_keys.into_iter().chain(new_keys.into_iter()).collect();
+    let old_keys = old_attributes.keys();
+    let new_keys = new_attributes.keys();
+    let all_keys: Vec<_> = old_keys.chain(new_keys).collect();
 
     let mut changed: HashMap<String, String> = HashMap::new();
     for key in all_keys {
-        if old_attributes.get(key) != new_attributes.get(key) {
-            if let Some(new_value) = new_attributes.get(key).cloned() {
-                changed.insert(key.clone(), new_value);
+        let old_value = old_attributes.get(key);
+        let new_value = new_attributes.get(key);
+
+        if old_value != new_value {
+            match new_value {
+                Some(new_value) => {
+                    changed.insert(key.clone(), new_value.clone());
+                }
+                None => {
+                    changed.insert(key.clone(), String::new());
+                }
             }
         }
     }
-    return changed;
+    changed
 }
