fa6d5df6a1fd6d2df2cd445c11eaa903f82bb089
Neil Dwyer <neildwyer1991@gmail.com>
2024-07-19 16:38:54 -0700

    remove unused

--- livekit/src/room/participant/mod.rs
+++ livekit/src/room/participant/mod.rs
@@ -153,7 +153,6 @@ pub(super) fn update_info(
     }
 
     let old_attributes = std::mem::replace(&mut info.attributes, new_info.attributes.clone());
-    // print new attributes
     let changed_attributes = utils::calculate_changed_attributes(
             old_attributes,
             new_info.attributes.clone(),
