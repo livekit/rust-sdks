19ac094aec4e476dd73600030e5501a794cedaf9
Kirill Bulatov <mail4score@gmail.com>
2024-11-12 17:39:55 +0100

    Fix dispatcher example compilation
    co-authored-by: Mikayla Maki <mikayla@zed.dev>

--- examples/basic_room_dispatcher/src/main.rs
+++ examples/basic_room_dispatcher/src/main.rs
@@ -47,12 +47,11 @@ fn main() {
             .unwrap();
 
         let (room, mut rx) = Room::connect(&url, &token, RoomOptions::default()).await.unwrap();
-        log::info!("Connected to room: {} - {}", room.name(), room.sid());
+        log::info!("Connected to room: {} - {}", room.name(), room.sid().await);
 
         room.local_participant()
             .publish_data(DataPacket {
                 payload: "Hello world".to_owned().into_bytes(),
-                kind: DataPacketKind::Reliable,
                 ..Default::default()
             })
             .await
