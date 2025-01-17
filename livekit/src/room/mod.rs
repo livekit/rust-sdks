5a214ea2db564d4d534e420d2630a2b627a75d02
lukasIO <mail@lukasseiler.de>
2025-01-17 15:09:07 +0100

    typo

--- livekit/src/room/mod.rs
+++ livekit/src/room/mod.rs
@@ -739,7 +739,7 @@ impl RoomSession {
                 self.handle_data_stream_chunk(chunk, participant_identity);
             }
             EngineEvent::DataStreamTrailer { trailer, participant_identity } => {
-                self.handle_data_stream_trailer(chunk, participant_identity);
+                self.handle_data_stream_trailer(trailer, participant_identity);
             }
             _ => {}
         }
