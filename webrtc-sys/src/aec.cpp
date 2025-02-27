6afe94e85cde94ac2378ddafbc45e8a3a649619b
Théo Monnom <theo.monnom@outlook.com>
2025-02-27 13:08:42 +0100

    Update aec.cpp

--- webrtc-sys/src/aec.cpp
+++ webrtc-sys/src/aec.cpp
@@ -4,6 +4,10 @@
 
 namespace livekit {
 
+bool SampleRateSupportsMultiBand(int sample_rate_hz) {
+  return sample_rate_hz == 32000 || sample_rate_hz == 48000;
+}
+
 Aec::Aec(const AecOptions& options) : options_(options) {
   aec3_ = std::make_unique<webrtc::EchoCanceller3>(
       webrtc::EchoCanceller3Config(), absl::nullopt, options.sample_rate,
@@ -32,7 +36,7 @@ void Aec::cancel_echo(int16_t* cap,
   cap_buf_->CopyFrom(cap, stream_cfg);
   rend_buf_->CopyFrom(rend, stream_cfg);
 
-  if (options_.sample_rate > 16000) {
+  if (SampleRateSupportsMultiBand(options_.sample_rate)) {
     cap_buf_->SplitIntoFrequencyBands();
     rend_buf_->SplitIntoFrequencyBands();
   }
@@ -41,14 +45,18 @@ void Aec::cancel_echo(int16_t* cap,
   aec3_->AnalyzeRender(rend_buf_.get());
   aec3_->ProcessCapture(cap_buf_.get(), false);
 
-  if (options_.sample_rate > 16000) {
+  if (SampleRateSupportsMultiBand(options_.sample_rate)) {
     cap_buf_->MergeFrequencyBands();
+    rend_buf_->SplitIntoFrequencyBands();
   }
 
   cap_buf_->CopyTo(stream_cfg, cap);
 }
 
 std::unique_ptr<Aec> create_aec(int sample_rate, int num_channels) {
+  if (sample_rate != 16000 && sample_rate != 32000 && sample_rate != 48000)
+    return nullptr;
+
   return std::make_unique<Aec>(AecOptions{sample_rate, num_channels});
 }
 
