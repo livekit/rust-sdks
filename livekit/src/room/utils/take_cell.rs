c6e0d71399d8b0242dab88811773bd12f6632477
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-04-05 11:47:16 +1100

    Refactor take cell
    Add method to conditionally take value based on predicate

--- livekit/src/room/utils/take_cell.rs
+++ livekit/src/room/utils/take_cell.rs
@@ -12,28 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use parking_lot::Mutex;
+use parking_lot::RwLock;
 use std::{fmt::Debug, sync::Arc};
 
 /// A thread-safe cell that holds a value which can be taken exactly once.
 /// After taking the value, subsequent attempts to take it will return `None`.
 pub struct TakeCell<T> {
-    inner: Arc<Mutex<Option<T>>>,
+    value: Arc<RwLock<Option<T>>>,
 }
 
 impl<T> TakeCell<T> {
     pub(crate) fn new(value: T) -> Self {
-        Self { inner: Arc::new(Mutex::new(Some(value))) }
+        Self { value: Arc::new(RwLock::new(Some(value))) }
     }
+
+    /// Take ownership of the value in the cell if it matches some predicate.
+    ///
+    /// This method will only take the value if the provided predicate returns `true` when called with the current value.
+    /// If the predicate returns `false` or the value has already been taken, this method returns `None`.
+    pub(crate) fn take_if_raw(&self, predicate: impl FnOnce(&T) -> bool) -> Option<T> {
+        if self.value.read().as_ref().map_or(false, |v| predicate(v)) {
+            self.take()
+        } else {
+            None
+        }
+    }
+
     /// Take ownership of the value in the cell. If the value has,
     /// already been taken, the result is `None`.
     pub fn take(&self) -> Option<T> {
-        self.inner.lock().take()
+        self.value.write().take()
     }
 
     /// Returns whether or not the value has been taken.
     pub fn is_taken(&self) -> bool {
-        self.inner.lock().is_none()
+        self.value.read().is_none()
     }
 }
 
@@ -42,7 +55,7 @@ where
     T: Debug,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let inner = self.inner.lock();
+        let inner = self.value.read();
         f.debug_struct("TakeCell")
             .field(
                 "value",
@@ -54,7 +67,7 @@ where
 
 impl<T> Clone for TakeCell<T> {
     fn clone(&self) -> Self {
-        Self { inner: self.inner.clone() }
+        Self { value: self.value.clone() }
     }
 }
 
@@ -68,5 +81,22 @@ mod tests {
         assert_eq!(cell.take(), Some(1));
         assert_eq!(cell.take(), None);
         assert_eq!(cell.is_taken(), true);
+    }
+
+    #[test]
+    fn test_take_if_raw() {
+        let cell = TakeCell::new(1);
+        assert_eq!(cell.take_if_raw(|value| *value == 2), None);
+        assert_eq!(cell.take_if_raw(|value| *value == 1), Some(1));
+        assert_eq!(cell.take_if_raw(|value| *value == 1), None);
+    }
+
+    #[test]
+    fn test_debug() {
+        let cell = TakeCell::new(1);
+        assert_eq!(format!("{:?}", cell), "TakeCell { value: 1 }");
+
+        cell.take();
+        assert_eq!(format!("{:?}", cell), "TakeCell { value: \"<taken>\" }");
     }
 }
