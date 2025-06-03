ba342c21f149269b2712505d572b63547268d5c8
CloudWebRTC <duanweiwei1982@gmail.com>
2025-06-03 18:03:48 +0800

    bump version for webrtc (fix win CI) (#650)
    * bump version for webrtc (fix win CI)
    
    * change for test.
    
    * revert changes.

--- webrtc-sys/build/src/lib.rs
+++ webrtc-sys/build/src/lib.rs
@@ -26,7 +26,7 @@ use regex::Regex;
 use reqwest::StatusCode;
 
 pub const SCRATH_PATH: &str = "livekit_webrtc";
-pub const WEBRTC_TAG: &str = "webrtc-7ec4c03";
+pub const WEBRTC_TAG: &str = "webrtc-ed96590";
 pub const IGNORE_DEFINES: [&str; 2] = ["CR_CLANG_REVISION", "CR_XCODE_VERSION"];
 
 pub fn target_os() -> String {
