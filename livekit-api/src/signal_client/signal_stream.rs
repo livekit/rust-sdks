9e7102ff8fd20af586de3a9aa070c40775150f78
jiyu-VERNECT <jy1001@virnect.com>
2025-06-09 08:15:47 +0900

    tls_config 파일 수정

--- livekit-api/src/signal_client/signal_stream.rs
+++ livekit-api/src/signal_client/signal_stream.rs
@@ -284,11 +284,13 @@ impl SignalStream {
                         }
 
                         // 3. TLS 설정 (단계별 빌더 호출)
-                        let tls_config = rustls::ClientConfig::builder()
-                            .with_safe_defaults()
+                       let tls_config = rustls::ClientConfig::builder()
+                            .with_safe_default_cipher_suites()
+                            .with_safe_default_kx_groups()
+                            .with_safe_default_protocol_versions()
+                            .unwrap()
                             .with_root_certificates(root_store)
                             .with_no_client_auth();
-
 
                         // 4. 서버 이름 검증
                         let server_name = ServerName::try_from(host).map_err(|_| {
