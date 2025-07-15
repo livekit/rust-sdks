ecc967e34a586909b9d0f908b1596d560c564757
David Chen <chenosaurus@gmail.com>
2025-07-15 13:34:26 -0700

    remove log

--- examples/local_audio/src/main.rs
+++ examples/local_audio/src/main.rs
@@ -173,7 +173,6 @@ fn find_output_device_by_name(name: &str) -> Result<Device> {
     let devices = host.output_devices()?;
     
     for device in devices {
-        println!("Device: {:?}", device);
         if let Ok(device_name) = device.name() {
             if device_name.contains(name) {
                 return Ok(device);
