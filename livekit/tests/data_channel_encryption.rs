7ee92bf8b8d2750835e92ada5b95fd56b4774d01
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-09-19 20:33:01 +1000

    Implement encryption test receiver side

--- livekit/tests/data_channel_encryption.rs
+++ livekit/tests/data_channel_encryption.rs
@@ -1,78 +1,69 @@
 #[cfg(feature = "__lk-e2e-test")]
 use {
     crate::common::test_rooms_with_options,
-    anyhow::{anyhow, Result},
+    anyhow::{Ok, Result},
     livekit::{
-        e2ee::key_provider::{KeyProvider, KeyProviderOptions},
-        DataPacket, E2eeOptions, RoomEvent, RoomOptions, SimulateScenario,
+        e2ee::{
+            key_provider::{KeyProvider, KeyProviderOptions},
+            EncryptionType,
+        },
+        DataPacket, E2eeOptions, RoomEvent, RoomOptions,
     },
-    std::{sync::Arc, time::Duration},
-    tokio::{sync::oneshot, time},
+    std::time::Duration,
+    tokio::{time::timeout, try_join},
 };
 
 mod common;
 
-// These tests depend on a LiveKit server, and thus are not enabled by default;
-// to run them, start a local LiveKit server in development mode, and enable the
-// E2E test feature:
-//
-// > livekit-server --dev
-// > cargo test --features __lk-e2e-test
-//
 #[cfg(feature = "__lk-e2e-test")]
 #[tokio::test]
 async fn test_reliable_retry_e2ee() -> Result<()> {
-    use livekit::e2ee::key_provider::KeyProvider;
-
     const ITERATIONS: usize = 128;
     const PAYLOAD_SIZE: usize = 4096;
 
-    let key_provider_1 =
-        KeyProvider::with_shared_key(KeyProviderOptions::default(), "password".as_bytes().to_vec());
-    let key_provider_2 =
+    let key_provider =
         KeyProvider::with_shared_key(KeyProviderOptions::default(), "password".as_bytes().to_vec());
 
-    // Set up test rooms
-    let mut options1 = RoomOptions::default();
-    options1.encryption = Some(E2eeOptions {
-        key_provider: key_provider_1,
-        encryption_type: livekit::e2ee::EncryptionType::Gcm,
-    });
+    let mut options = RoomOptions::default();
+    options.encryption = Some(E2eeOptions { key_provider, encryption_type: EncryptionType::Gcm });
 
-    let mut options2 = RoomOptions::default();
-    options2.encryption = Some(E2eeOptions {
-        key_provider: key_provider_2,
-        encryption_type: livekit::e2ee::EncryptionType::Gcm,
-    });
+    let mut rooms = test_rooms_with_options([options.clone(), options]).await?;
 
-    let options = vec![options1, options2];
-    let mut rooms = test_rooms_with_options(options).await?;
     let (sending_room, _) = rooms.pop().unwrap();
     let (receiving_room, mut receiving_event_rx) = rooms.pop().unwrap();
 
     sending_room.e2ee_manager().set_enabled(true);
     receiving_room.e2ee_manager().set_enabled(true);
 
-    let sending_room = Arc::new(sending_room);
-    let receiving_room = Arc::new(receiving_room);
+    let send_packets = async move {
+        for iteration in 0..ITERATIONS {
+            let packet = DataPacket {
+                reliable: true,
+                // Set all the bytes in the payload equal to the iteration number
+                // to verify on the receiver side.
+                payload: [iteration as u8; PAYLOAD_SIZE].to_vec(),
+                ..Default::default()
+            };
+            sending_room.local_participant().publish_data(packet).await?;
+        }
+        Ok(())
+    };
 
-    let receiving_identity = receiving_room.local_participant().identity();
-    let (fulfill, expectation) = oneshot::channel();
+    let receive_packets = async move {
+        let mut recv_idx = 0;
+        while let Some(event) = receiving_event_rx.recv().await {
+            let RoomEvent::DataReceived { payload, .. } = event else {
+                continue;
+            };
+            assert!(payload.iter().all(|byte| *byte == recv_idx as u8));
+            recv_idx += 1;
+            if recv_idx >= ITERATIONS {
+                break;
+            }
+        }
+        Ok(())
+    };
 
-    for _ in 0..ITERATIONS {
-        let packet = DataPacket {
-            reliable: true,
-            payload: [0xFA; PAYLOAD_SIZE].to_vec(),
-            destination_identities: vec![receiving_identity.clone()],
-            ..Default::default()
-        };
-        sending_room.local_participant().publish_data(packet).await?;
-        time::sleep(Duration::from_millis(10)).await;
-    }
-
-    match time::timeout(Duration::from_secs(15), expectation).await {
-        Ok(Ok(())) => Ok(()),
-        Ok(Err(_)) => Err(anyhow!("Not all packets were received")),
-        Err(_) => Err(anyhow!("Timed out waiting for packets")),
-    }
+    timeout(Duration::from_secs(5), async { try_join!(send_packets, receive_packets) }).await??;
+    Ok(())
 }
