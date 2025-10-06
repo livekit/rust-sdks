701962c4f7dae00b824ba15026bda07e91607860
David Zhao <dz@livekit.io>
2025-10-05 22:06:42 -0700

    fix lock

--- livekit/src/rtc_engine/mod.rs
+++ livekit/src/rtc_engine/mod.rs
@@ -450,13 +450,17 @@ impl EngineInner {
                 match action {
                     proto::leave_request::Action::Resume
                     | proto::leave_request::Action::Reconnect => {
-                        let running_handle = self.running_handle.read();
+                        {
+                            let running_handle = self.running_handle.read();
 
-                        // server could have sent a leave & disconnected signal client
-                        // we don't want to start another resume cycle
-                        if !running_handle.can_reconnect {
-                            return Ok(());
+                            // server could have sent a leave & disconnected signal client
+                            // we don't want to start another resume cycle
+                            if !running_handle.can_reconnect {
+                                return Ok(());
+                            }
+                            // ensure we release the lock from this scope, it'll be used again in reconnection_needed
                         }
+                        
                         log::warn!(
                             "received session close: {:?} {:?} {:?}",
                             source,
