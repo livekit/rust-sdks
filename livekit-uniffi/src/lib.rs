648a0a22d3099e05e9a7870110594867018d82d3
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-10-16 17:54:20 +1100

    Expose access token module

--- livekit-uniffi/src/lib.rs
+++ livekit-uniffi/src/lib.rs
@@ -1,5 +1,20 @@
-fn add(a: u32, b: u32) -> u32 {
-    a + b
-}
+// Copyright 2025 LiveKit, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-uniffi::include_scaffolding!("math");
+/// Access token generation and verification from [`livekit-api::access_token`].
+pub mod access_token;
+
+// TODO: expose more modules...
+
+uniffi::setup_scaffolding!();
