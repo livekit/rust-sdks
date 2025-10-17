b4c79ccb0fc59aa58e1a5d58e020e78045a95731
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-10-17 11:45:01 +0200

    Lipo

--- livekit-uniffi/swift.sh
+++ livekit-uniffi/swift.sh
@@ -22,10 +22,22 @@ cargo build --release --target aarch64-apple-ios
 cargo build --release --target aarch64-apple-ios-sim
 cargo build --release --target x86_64-apple-ios
 
-# Combine iOS Simulator here
+mkdir -p ../target/ios-simulator/release
+lipo -create \
+    ../target/aarch64-apple-ios-sim/release/liblivekit_uniffi.a \
+    ../target/x86_64-apple-ios/release/liblivekit_uniffi.a \
+    -output ../target/ios-simulator/release/liblivekit_uniffi.a
+
+mkdir -p ../target/macos/release
+lipo -create \
+    ../target/aarch64-apple-darwin/release/liblivekit_uniffi.a \
+    ../target/x86_64-apple-darwin/release/liblivekit_uniffi.a \
+    -output ../target/macos/release/liblivekit_uniffi.a
+
+rm -rf ../target/LiveKitFFI.xcframework
 
 xcodebuild -create-xcframework \
-    -library ../target/aarch64-apple-ios-sim/release/liblivekit_uniffi.a -headers ./generated/swift \
     -library ../target/aarch64-apple-ios/release/liblivekit_uniffi.a -headers ./generated/swift \
-    -library ../target/aarch64-apple-darwin/release/liblivekit_uniffi.a -headers ./generated/swift \
+    -library ../target/ios-simulator/release/liblivekit_uniffi.a -headers ./generated/swift \
+    -library ../target/macos/release/liblivekit_uniffi.a -headers ./generated/swift \
     -output "../target/LiveKitFFI.xcframework"
