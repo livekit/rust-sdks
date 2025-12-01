89a2b85caac8e6925ae815e64316f3e0da865f7a
Iason Paraskevopoulos <iaswnparaskev@gmail.com>
2025-11-30 19:37:17 -0800

    Expose desktop capturer (#725)
    * feat: expose DesktopCapturer
    
    Implements screen sharing by exposing libwebrtc's
    DesktopCapturer.
    
    A few platform specific notes:
      - macos:
        * It is using screen capture kit and the system picker by
          default. If the system picker is disabled then get_sources
          returns an empty list when trying to capture a display. The
          display native id needs to be acquired using different means
          from the client.
      - linux:
        * With pipewire the only way to select window or display is via
          the system picker.
    
    * use pkg-config to find Linux libraries (#2)
    
    * Support x11 dependencies
    
    * Modify example to work on x11
    
    * Address comments
    
    * Properly guard sck options
    
    * Address review comments
    
    In particular:
       - Adds support for the generic capturer.
       - Exposes the desktop_capturer module only on macos, linux and
         windows.
       - Updates the example
    
    * Update license
    
    * Set callback on start
    
    * Make the callback FnMut
    
    * cpp formatting
    
    * Allow disabling system picker in example
    
    * Address review comments
    
    * Remove reference to temporary
    
    * Run screen-sharing example only on supported platforms
    
    * cargo fmt
    
    * Update build runner with DesktopCapturer deps on linux
    
    * Update runner
    
    * Update tests runner
    
    * fix Android build (#7)
    
    * screensharing example: remove rustls-native feature from livekit
    
    This was breaking the Android builds.
    
    * improve error handling for File::open
    
    * webrtc-sys-build: skip parsing desktop_capture.ninja on Android
    
    * webrtc-sys-build: bump prebuilt library to webrtc-0001d84-2
    
    * Enable TLS features for the screenshare example
    
    ---------
    
    Co-authored-by: Be <be.0@gmx.com>

--- webrtc-sys/build.rs
+++ webrtc-sys/build.rs
@@ -49,6 +49,7 @@ fn main() {
         "src/android.rs",
         "src/prohibit_libsrtp_initialization.rs",
         "src/apm.rs",
+        "src/desktop_capturer.rs",
     ]);
 
     builder.files(&[
@@ -77,6 +78,7 @@ fn main() {
         "src/global_task_queue.cpp",
         "src/prohibit_libsrtp_initialization.cpp",
         "src/apm.cpp",
+        "src/desktop_capturer.cpp",
     ]);
 
     let webrtc_dir = webrtc_sys_build::webrtc_dir();
@@ -152,6 +154,28 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib=dl");
             println!("cargo:rustc-link-lib=dylib=pthread");
             println!("cargo:rustc-link-lib=dylib=m");
+
+            // In order to avoid any ABI mismatches we use the sysroot's headers.
+            add_gio_headers(&mut builder);
+
+            #[cfg(target_os = "linux")]
+            {
+                for lib_name in [
+                    "libdrm",
+                    "gbm",
+                    "x11",
+                    "xfixes",
+                    "xdamage",
+                    "xrandr",
+                    "xcomposite",
+                    "xext",
+                    "glib-2.0",
+                    "gobject-2.0",
+                    "gio-2.0",
+                ] {
+                    pkg_config::probe_library(lib_name).unwrap();
+                }
+            }
 
             let x86 = target_arch == "x86_64" || target_arch == "i686";
             let arm = target_arch == "aarch64" || target_arch.contains("arm");
@@ -361,4 +385,30 @@ fn configure_android_sysroot(builder: &mut cc::Build) {
     let toolchain = webrtc_sys_build::android_ndk_toolchain().unwrap();
     let sysroot = toolchain.join("sysroot").canonicalize().unwrap();
     builder.flag(format!("-isysroot{}", sysroot.display()).as_str());
+}
+
+fn add_gio_headers(builder: &mut cc::Build) {
+    let webrtc_dir = webrtc_sys_build::webrtc_dir();
+    let target_arch = webrtc_sys_build::target_arch();
+    let target_arch_sysroot = match target_arch.as_str() {
+        "arm64" => "arm64",
+        "x64" => "amd64",
+        _ => panic!("unsupported arch"),
+    };
+    let sysroot_path = format!("include/build/linux/debian_bullseye_{target_arch_sysroot}-sysroot");
+    let sysroot = webrtc_dir.join(sysroot_path);
+    let glib_path = sysroot.join("usr/include/glib-2.0");
+    println!("cargo:info=add_gio_headers {}", glib_path.display());
+
+    builder.include(&glib_path);
+    let arch_specific_path = match target_arch.as_str() {
+        "x64" => "x86_64-linux-gnu",
+        "arm64" => "aarch64-linux-gnu",
+        _ => panic!("unsupported target"),
+    };
+
+    let glib_path_config = sysroot.join("usr/lib");
+    let glib_path_config = glib_path_config.join(arch_specific_path);
+    let glib_path_config = glib_path_config.join("glib-2.0/include");
+    builder.include(&glib_path_config);
 }
