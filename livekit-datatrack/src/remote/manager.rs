5fbbe848ea2aaf71588611da9e55476bb194298b
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-12-23 14:54:00 +1100

    Handle packet input

--- livekit-datatrack/src/remote/manager.rs
+++ livekit-datatrack/src/remote/manager.rs
@@ -14,6 +14,7 @@
 
 use crate::{DataTrack, DecryptionProvider, InternalError, Remote};
 use anyhow::Context;
+use bytes::Bytes;
 use from_variants::FromVariants;
 use livekit_protocol as proto;
 use std::sync::Arc;
@@ -59,6 +60,7 @@ pub struct SubManagerOptions {
 /// Manager for remote data tracks.
 pub struct Manager {
     signal_in_tx: mpsc::Sender<SubSignalInput>,
+    packet_in_tx: mpsc::Sender<Bytes>,
     // sub request
 }
 
@@ -76,13 +78,15 @@ impl Manager {
         let (signal_in_tx, signal_in_rx) = mpsc::channel(Self::CH_BUFFER_SIZE);
         let (signal_out_tx, signal_out_rx) = mpsc::channel(Self::CH_BUFFER_SIZE);
         let (track_out_tx, track_out_rx) = mpsc::channel(Self::CH_BUFFER_SIZE);
+        let (packet_in_tx, packet_in_rx) = mpsc::channel(Self::CH_BUFFER_SIZE);
 
-        let manager = Manager { signal_in_tx };
+        let manager = Manager { signal_in_tx, packet_in_tx };
         let task = ManagerTask {
             decryption: options.decryption,
             signal_in_rx,
             signal_out_tx,
             track_out_tx,
+            packet_in_rx
         };
 
         let signal_out_stream = ReceiverStream::new(signal_out_rx);
@@ -99,6 +103,11 @@ impl Manager {
     pub fn handle_signal(&self, message: SubSignalInput) -> Result<(), InternalError> {
         Ok(self.signal_in_tx.try_send(message).context("Failed to handle signal input")?)
     }
+
+    /// Handles a packet received over the transport.
+    pub fn handle_packet(&self, packet: Bytes) -> Result<(), InternalError> {
+        Ok(self.packet_in_tx.try_send(packet).context("Failed to packet")?)
+    }
 }
 
 pub struct ManagerTask {
@@ -106,24 +115,53 @@ pub struct ManagerTask {
     signal_in_rx: mpsc::Receiver<SubSignalInput>,
     signal_out_tx: mpsc::Sender<SubSignalOutput>,
     track_out_tx: mpsc::Sender<DataTrack<Remote>>,
+    packet_in_rx: mpsc::Receiver<Bytes>
 }
 
 impl ManagerTask {
     pub async fn run(mut self) -> Result<(), InternalError> {
-        Ok(())
+        loop {
+            tokio::select! {
+                biased; // Handle signal input first
+                // TODO: check cancellation
+                Some(signal) = self.signal_in_rx.recv() => self.handle_signal(signal),
+                Some(packet) = self.packet_in_rx.recv() => self.handle_packet(packet),
+                else => Ok(())
+            }
+            .inspect_err(|err| log::error!("{}", err))
+            .ok();
+        }
+    }
+
+    fn handle_packet(&mut self, packet: Bytes) -> Result<(), InternalError> {
+        todo!()
+    }
+
+    fn handle_signal(&mut self, message: SubSignalInput) -> Result<(), InternalError> {
+        match message {
+            SubSignalInput::ParticipantUpdate(message) => self.handle_participant_update(message),
+            SubSignalInput::SubscriberHandles(message) => self.handle_subscriber_handles(message)
+        }
+    }
+
+    fn handle_participant_update(&mut self, message: proto::ParticipantUpdate) -> Result<(), InternalError> {
+        todo!()
+    }
+
+    fn handle_subscriber_handles(&mut self, message: proto::DataTrackSubscriberHandles) -> Result<(), InternalError> {
+        todo!()
     }
 }
 
-/// Signal message produced by [`SubManager`] to be forwarded to the SFU.
+/// Signal message produced by [`Manager`] to be forwarded to the SFU.
 #[derive(Debug, FromVariants)]
 pub enum SubSignalOutput {
     UpdateSubscription(proto::UpdateDataSubscription),
 }
 
-/// Signal message received from the SFU handled by [`SubManager`].
+/// Signal message received from the SFU handled by [`Manager`].
 #[derive(Debug, FromVariants)]
 pub enum SubSignalInput {
     ParticipantUpdate(proto::ParticipantUpdate),
-    UnpublishResponse(proto::UnpublishDataTrackResponse),
     SubscriberHandles(proto::DataTrackSubscriberHandles),
 }
