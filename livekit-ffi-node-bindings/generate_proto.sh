d36878ba9b5d7637598f612fff913860f1993b93
lukasIO <mail@lukasseiler.de>
2026-01-12 12:32:28 +0100

    Add license headers

--- livekit-ffi-node-bindings/generate_proto.sh
+++ livekit-ffi-node-bindings/generate_proto.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-
-# SPDX-FileCopyrightText: 2024 LiveKit, Inc.
+# Copyright 2025 LiveKit, Inc.
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # This script requires protobuf-compiler and https://www.npmjs.com/package/@bufbuild/protoc-gen-es
 # `brew install protobuf-c && npm install -g @bufbuild/protoc-gen-es@2.2.0`
