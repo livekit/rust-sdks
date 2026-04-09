95bf51c5b8e213c164ce4fa3b19667be7d4ce124
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-04-09 15:16:51 +0900

    Create module

--- livekit-uniffi/src/data_track/mod.rs
+++ livekit-uniffi/src/data_track/mod.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 LiveKit, Inc.
+// Copyright 2026 LiveKit, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,16 +12,3 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/// Data tracks core from [`livekit-datatrack`].
-pub mod data_track;
-
-/// Access token generation and verification from [`livekit-api::access_token`].
-pub mod access_token;
-
-/// Forward log messages from Rust.
-pub mod log_forward;
-
-/// Information about the build such as version.
-pub mod build_info;
-
-uniffi::setup_scaffolding!();
