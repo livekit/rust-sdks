1a2209045d542604b01aaa7cd6ec42d83df0a749
davidliu <davidliu@deviange.net>
2026-05-19 22:16:03 +0900

    maven publishing

--- livekit-uniffi/support/android/build.gradle.kts
+++ livekit-uniffi/support/android/build.gradle.kts
@@ -1,7 +1,16 @@
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.plugins.signing.SigningExtension
+
 plugins {
     id("com.android.library") version "8.2.2"
     id("org.jetbrains.kotlin.android") version "1.9.22"
+    `maven-publish`
+    signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
+
+group = providers.gradleProperty("GROUP").get()
+version = providers.gradleProperty("VERSION_NAME").get()
 
 val generatedKotlinDir = rootDir.resolve("../../packages/kotlin")
 
@@ -34,6 +43,12 @@ android {
         jvmTarget = "17"
     }
 
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+
     sourceSets {
         getByName("main") {
             if (generatedKotlinDir.exists()) {
@@ -46,4 +61,62 @@ android {
 dependencies {
     implementation("net.java.dev.jna:jna:5.16.0@aar")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+}
+
+afterEvaluate {
+    extensions.configure<PublishingExtension>("publishing") {
+        publications {
+            register<MavenPublication>("release") {
+                from(components["release"])
+                artifactId = rootProject.name
+
+                pom {
+                    name.set("LiveKit UniFFI Android")
+                    description.set(providers.gradleProperty("POM_DESCRIPTION").get())
+                    url.set(providers.gradleProperty("POM_URL").get())
+                    licenses {
+                        license {
+                            name.set(providers.gradleProperty("POM_LICENCE_NAME").get())
+                            url.set(providers.gradleProperty("POM_LICENCE_URL").get())
+                            distribution.set(providers.gradleProperty("POM_LICENCE_DIST").get())
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set(providers.gradleProperty("POM_DEVELOPER_ID").get())
+                            name.set(providers.gradleProperty("POM_DEVELOPER_NAME").get())
+                        }
+                    }
+                    scm {
+                        connection.set(providers.gradleProperty("POM_SCM_CONNECTION").get())
+                        developerConnection.set(providers.gradleProperty("POM_SCM_DEV_CONNECTION").get())
+                        url.set(providers.gradleProperty("POM_SCM_URL").get())
+                    }
+                }
+            }
+        }
+    }
+
+    val releaseSigningEnabled =
+        providers
+            .gradleProperty("RELEASE_SIGNING_ENABLED")
+            .map(String::toBooleanStrict)
+            .getOrElse(true)
+    if (releaseSigningEnabled) {
+        extensions.configure<SigningExtension>("signing") {
+            sign(publishing.publications["release"])
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        register("nexus") {
+            nexusUrl.set(uri(providers.gradleProperty("RELEASE_REPOSITORY_URL").get()))
+            snapshotRepositoryUrl.set(uri(providers.gradleProperty("SNAPSHOT_REPOSITORY_URL").get()))
+            providers.gradleProperty("STAGING_PROFILE_ID").orNull?.takeIf { it.isNotBlank() }?.let { profileId ->
+                stagingProfileId.set(profileId)
+            }
+        }
+    }
 }
