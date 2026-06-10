21cb0b4d511ada6bae634f1460374ff5f11b28c9
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-06-10 13:00:18 -0700

    Add crate READMEs (#1152)
    Show basic information on crates.io and in generated docs.

--- livekit-api/src/lib.rs
+++ livekit-api/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![doc = include_str!("../README.md")]
+
 #[cfg(feature = "access-token")]
 pub mod access_token;
 
