6c9fd0930806bf7dac6d6efe942eef43213f7f0a
Ryan Gaus <ryan.gaus@livekit.io>
2026-07-01 12:47:42 -0400

    fix: run cargo fmt

--- livekit/src/prelude.rs
+++ livekit/src/prelude.rs
@@ -22,8 +22,8 @@ pub use crate::{
     },
     id::*,
     participant::{
-        ConnectionQuality, DisconnectReason, LocalParticipant, Participant,
-        PerformRpcData, RemoteParticipant, RpcError, RpcErrorCode, RpcInvocationData,
+        ConnectionQuality, DisconnectReason, LocalParticipant, Participant, PerformRpcData,
+        RemoteParticipant, RpcError, RpcErrorCode, RpcInvocationData,
     },
     publication::{LocalTrackPublication, RemoteTrackPublication, TrackPublication},
     track::{
