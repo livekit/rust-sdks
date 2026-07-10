20ab7293813c8092c48ac848735e70be3bcd7cf0
lukasIO <mail@lukasseiler.de>
2026-07-10 09:45:10 +0200

    just a test

--- libwebrtc/src/lib.rs
+++ libwebrtc/src/lib.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// this is a test change
+
 use thiserror::Error;
 
 #[cfg_attr(target_arch = "wasm32", path = "web/mod.rs")]
